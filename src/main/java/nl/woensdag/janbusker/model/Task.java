32aeb7b0b680e456cdec00a23a01285deef5373b
Keyslam <justinvanderleij@gmail.com>
2024-05-29 14:59:19 +0200

    Add fields to Task model

--- src/main/java/nl/woensdag/janbusker/model/Task.java
+++ src/main/java/nl/woensdag/janbusker/model/Task.java
@@ -2,11 +2,38 @@ package nl.woensdag.janbusker.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Task {
     @Id()
-    @GeneratedValue
-    public Long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description = "";
+
+    private boolean isCompleted = false;
+
+    public Task() { }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void setCompleted(boolean completed) {
+        isCompleted = completed;
+    }
 }
