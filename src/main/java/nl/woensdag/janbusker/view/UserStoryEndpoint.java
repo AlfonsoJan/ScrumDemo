65013a10003d1538aafccfd854d44314fc962ebd
TSpoon <emielangelaan@gmail.com>
2024-05-30 12:46:28 +0200

    dit is een comment

--- src/main/java/nl/woensdag/janbusker/view/UserStoryEndpoint.java
+++ src/main/java/nl/woensdag/janbusker/view/UserStoryEndpoint.java
@@ -1,4 +1,20 @@
 package nl.woensdag.janbusker.view;
 
+import nl.woensdag.janbusker.model.Schip;
+import nl.woensdag.janbusker.model.UserStory;
+import nl.woensdag.janbusker.service.SchipService;
+import nl.woensdag.janbusker.service.UserStoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserStoryEndpoint {
+    @Autowired
+    private SchipService userStoryService;
+
+    @GetMapping("getUserStory")
+    public Iterable<Schip> getUserStory() {
+        return userStoryService.allShips();
+    }
 }
